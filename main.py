efb1167517c79acd0e5ff52d6b47246e0c90f83b
Massasquash <fantasy5512.fun@gmail.com>
2020-06-11 23:02:31 +0900

    Set config vars

--- main.py
+++ main.py
@@ -13,18 +13,31 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 
 # Google Drive APIの認証
-filename = 'gcp_credentials.json'
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-credentials = ServiceAccountCredentials.from_json_keyfile_name(filename, scope)
+
+credential_dict = {
+  "type": "service_account",
+  "project_id": os.environ['SHEET_PROJECT_ID'],
+  "private_key_id": os.environ['SHEET_PRIVATE_KEY_ID'],
+  "private_key": os.environ['SHEET_PRIVATE_KEY'],
+  "client_email": os.environ['SHEET_CLIENT_EMAIL'],
+  "client_id": os.environ['SHEET_CLIENT_ID'],
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": os.environ['SHEET_CLIENT_X509_CERT_URL']
+}
+
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(credential_dict, scope)
 
 gc = gspread.authorize(credentials)
 
-SPREADSHEET_KEY = '1OqyYfjv7cLs4SNd-c3yfD0E2ibvBkvyAviqw6P1CnOU'
-worksheet = gc.open_by_key(SPREADSHEET_KEY).sheet1
+spreadsheet = os.environ['SPREADSHEET_KEY']
+worksheet = gc.open_by_key(spreadsheet).sheet1
 
 # LINEbotの認証
-channel_secret = os.environ["CHANNEL_SECRET"]
-channel_access_token = os.environ["CHANNEL_ACCESS_TOKEN"]
+channel_secret = os.environ["LINE_CHANNEL_SECRET"]
+channel_access_token = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 
 # flaskアプリ実装
 app = Flask(__name__)
