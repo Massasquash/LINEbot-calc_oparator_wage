b9942e57996a997dc4d2d9d7c8779c82814b380e
Massasquash <fantasy5512.fun@gmail.com>
2020-06-13 17:58:59 +0900

    Set config vars

---
+++ api_settings.py
@@ -0,0 +1,21 @@
+import os
+from oauth2client.service_account import ServiceAccountCredentials
+
+# Google Drive APIの認証を実行
+def set_google_credentials():
+  scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+  credential_dict = {
+    "type": "service_account",
+    "project_id": os.environ['SHEET_PROJECT_ID'],
+    "private_key_id": os.environ['SHEET_PRIVATE_KEY_ID'],
+    "private_key": os.environ['SHEET_PRIVATE_KEY'],
+    "client_email": os.environ['SHEET_CLIENT_EMAIL'],
+    "client_id": os.environ['SHEET_CLIENT_ID'],
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": os.environ['SHEET_CLIENT_X509_CERT_URL']
+  }
+
+  credentials = ServiceAccountCredentials.from_json_keyfile_dict(credential_dict, scope)
+  return credentials
--- app.py
+++ app.py
@@ -9,41 +9,20 @@ from linebot.models import (
 )
 from linebot.exceptions import InvalidSignatureError
 
-from oauth2client.service_account import ServiceAccountCredentials
+import api_settings
 
+#LINE botの設定
+line_bot_api = LineBotApi(os.environ["LINE_CHANNEL_ACCESS_TOKEN"])
+handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET"])
 
-# Google Drive APIの認証
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-
-credential_dict = {
-  "type": "service_account",
-  "project_id": os.environ['SHEET_PROJECT_ID'],
-  "private_key_id": os.environ['SHEET_PRIVATE_KEY_ID'],
-  "private_key": os.environ['SHEET_PRIVATE_KEY'],
-  "client_email": os.environ['SHEET_CLIENT_EMAIL'],
-  "client_id": os.environ['SHEET_CLIENT_ID'],
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": os.environ['SHEET_CLIENT_X509_CERT_URL']
-}
-
-credentials = ServiceAccountCredentials.from_json_keyfile_dict(credential_dict, scope)
-
+# Spreadsheet連携
+credentials = api_settings.set_google_credentials()
 gc = gspread.authorize(credentials)
-
 spreadsheet = os.environ['SPREADSHEET_KEY']
 worksheet = gc.open_by_key(spreadsheet).sheet1
 
-# LINEbotの認証
-channel_secret = os.environ["LINE_CHANNEL_SECRET"]
-channel_access_token = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
-
-# flaskアプリ実装
+# flaskアプリ実装（初期化）
 app = Flask(__name__)
-
-line_bot_api = LineBotApi(channel_access_token)
-handler = WebhookHandler(channel_secret)
 
 @app.route('/')
 def hello_world():
