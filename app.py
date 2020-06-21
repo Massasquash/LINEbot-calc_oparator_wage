57042201646e14cd25b1a2f04ee4e3d41c20ee16
Massasquash <fantasy5512.fun@gmail.com>
2020-06-21 21:43:28 +0900

    Calculate work minutes when get work report

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, abort
-import os, gspread
+import os
+from datetime import datetime as dt
+import gspread
 
 from linebot import LineBotApi, WebhookHandler
 from linebot.models import (
@@ -14,8 +16,9 @@ from linebot.exceptions import InvalidSignatureError
 import api_settings, tools, actions
 
 #LINE botの設定
-line_bot_api = LineBotApi(os.environ["LINE_CHANNEL_ACCESS_TOKEN"])
-handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET"])
+#TODO:本番環境への移行時はDEVのついていない環境変数を使用する
+line_bot_api = LineBotApi(os.environ["DEV_LINE_CHANNEL_ACCESS_TOKEN"])
+handler = WebhookHandler(os.environ["DEV_LINE_CHANNEL_SECRET"])
 
 # Spreadsheet連携
 credentials = api_settings.set_google_credentials()
@@ -29,7 +32,7 @@ master_sheet = ss.worksheet('master')
 # マスター情報の取得
 machines, persons = tools.get_master_data()
 
-# TODO:あとで修正
+# TODO:cacheあとで修正
 cache_sheet = ss.worksheet('cache')
 
 # flaskアプリ実装（初期化）
@@ -70,10 +73,9 @@ def handle_message(event):
 def handle_postback(event):
     ev_data = event.postback.data
 
-    if ev_data == 'starttime':
+    if ev_data == 'starttime': 
         starttime = event.postback.params['datetime']
         cache_sheet.update_acell('B2', starttime)
-        # actions.reply_text(event, f'開始時間は{starttime}')
         actions.send_end_datetime_picker(event)
         return
 
@@ -94,21 +96,37 @@ def handle_postback(event):
         cache_sheet.update_acell('B5', person)
         cache = cache_sheet.get('B1:B5')
         
-        msg = f'''
-        作業開始：{cache[1][0]}\n\
-        作業終了：{cache[2][0]}\n\
-        コンバイン名：{cache[3][0]}\n\
-        作業者名：{cache[4][0]}\n\n\
-        この日報を登録しますか？
-        '''
+        starttime = dt.strptime(cache[1][0], '%Y-%m-%dT%H:%M')
+        endtime = dt.strptime(cache[2][0], '%Y-%m-%dT%H:%M')
 
+        if starttime >= endtime:
+            actions.reply_text(event, '終了時間は開始時間より後に設定してください。')
+            return
+
+        timedelta = endtime - starttime
+        seconds = timedelta.total_seconds()
+        round_up_minutes = (seconds // 900 + 1) * 900 / 60
+        starttime = starttime.strftime('%Y/%m/%d %H:%M')
+        endtime = endtime.strftime('%Y/%m/%d %H:%M')
+        disp_totaltime = f'{round_up_minutes // 60}時間{round_up_minutes % 60}分'
+        cache_sheet.update_acell('B6', round_up_minutes)
+
+        msg = f'''
+        作業開始：{starttime}\n
+        作業終了：{endtime}\n
+        合計作業時間：{disp_totaltime}
+        コンバイン名：{cache[3][0]}\n
+        作業者名：{cache[4][0]}\n\n
+この日報を登録しますか？
+（※合計作業時間は15分単位・15分に満たない場合は切り上げで算出）
+        '''
         actions.send_entry_quickreply(event, msg)
         return
 
     if ev_data.startswith('entry'):
         if ev_data[-1] == '0':
-            cache = cache_sheet.get('B1:B5')
-            record_sheet.append_row([cache[1][0], cache[2][0], cache[3][0], cache[4][0]])
+            cache = cache_sheet.get('B1:B6')
+            record_sheet.append_row([cache[1][0], cache[2][0], cache[3][0], cache[4][0], cache[5][0]])
             msg = '日報を登録しました！'
             actions.reply_text(event, msg)
             return
